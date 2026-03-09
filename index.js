419bc78aa6abd86337c8945fd003de8481bd6c6c
Ivan Krivonos <krivonos.ivann@gmail.com>
2026-03-09 19:21:45 +0300

    add queue

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import { Queue } from "./queue.js";
 const MESSAGES_LENGTH = 30;
 
 const callback = async (message) => {
-    setTimeout(() => {console.log(message)}, 1000);
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    console.log(message);
 }
 
 const queue = new Queue('my_queue', callback, 3);
@@ -12,3 +13,7 @@ console.log(queue);
 for(let i = 0; i < MESSAGES_LENGTH; i++) {
     queue.addTask(`task ${i}`);
   }
+
+setTimeout(() => {
+    queue.addTask('task 30');
+}, 1000);
--- queue.js
+++ queue.js
@@ -1,20 +1,38 @@
 export class Queue {
-      tasks = [];
 
-      constructor (_name, _callback, _maxTasks = 1){
-
+      constructor (name, callback, maxTasks = 1){
+            this._name = name;
+            this._callback = callback;
+            this._maxTasks = maxTasks;
+            this._activeTasks = 0;
+            this._tasks = [];
+            this.itWorks = false;
       }
 
       get name() {
             return this._name;
         }
+      
+      _canWork() {
+            return !this.itWorks || this._activeTasks < this._maxTasks;
+      }
 
       addTask(task) {
-            this.tasks.push(task);
-            this.crawl();
+            this._tasks.push(task);
+            if(this._canWork()){
+                  this.crawl();
+            }
         }
 
       crawl() {
-            this._callback(this.tasks.shift());
+            if(this._tasks.length > 0) {
+                  this.itWorks = true;
+                  this._activeTasks++;
+                  this._callback(this._tasks.shift()).finally(() => {
+                        this.itWorks = false;
+                        this._activeTasks--;
+                        this.crawl();
+                  });
+            } 
         }
 }
